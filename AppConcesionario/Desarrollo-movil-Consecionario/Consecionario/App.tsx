433b290d577dcc8d955bc5f1acc0f0daca9c9f9f
JonnyCoder26 <161879564+JonnyCoder26@users.noreply.github.com>
2024-04-27 08:45:05 -0500

    entrega 2 corregida

--- AppConcesionario/Desarrollo-movil-Consecionario/Consecionario/App.tsx
+++ AppConcesionario/Desarrollo-movil-Consecionario/Consecionario/App.tsx
@@ -2,13 +2,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './components/HomeScreen';
-import EntryForms from './components/EntryForms';
-import OptionsNavigation from './components/OptionsNavigation';
-import Catalog from './components/Catalog';
-import Details from './components/Details';
-
-
+import HomeScreen from './src/Components/HomeScreen';
+import EntryForms from './src/Components/EntryForms';
+import OptionsNavigation from './src/Components/OptionsNavigation';
+import Catalog from './src/Components/Catalog';
+import Details from './src/Components/Details';
 
 
 
@@ -20,7 +18,7 @@ const App = () => {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="EntryForms" component={EntryForms} />
-        <Stack.Screen name="OptionsNavigation" component={OptionsNavigation} />        
+        <Stack.Screen name="OptionsNavigation" component={OptionsNavigation} />
         <Stack.Screen name="Catalog" component={Catalog} />
         <Stack.Screen name="Details" component={Details} />
 
