ba6c8212487514764bf9e9320a596fcb323802f2
JonnyCoder26 <161879564+JonnyCoder26@users.noreply.github.com>
2024-04-30 17:07:49 -0500

    Entrega 2

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './components/HomeScreen';
+import EntryForms from './components/EntryForms';
+import OptionsNavigation from './components/OptionsNavigation';
+import Catalog from './components/Catalog';
+import Details from './components/Details';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebaseConfig';
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="EntryForms" component={EntryForms} />
+        <Stack.Screen name="OptionsNavigation" component={OptionsNavigation} />        
+        <Stack.Screen name="Catalog" component={Catalog} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ firebase.js
@@ -0,0 +1,9 @@
+// firebase.js
+import * as firebase from "firebase/app";
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
